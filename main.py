c7a7127a30c156ebb0829a9c806b57bca88f14bf
Théo GUEURET <tgueuret@live.fr>
2021-09-07 00:38:00 +0200

    game loop + checks + peices movement

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ from GameEngine import GameEngine
 def main():
     print("WELCOME IM MY CHESS GAME  ---- by Theo GUEURET")
     myGame = GameEngine()
-    #myGame.printAllPieces()
-    #myGame.printAllSquares()
-    #test de feature
-    myGame.printBoard()
-    for piece in myGame.board.pieces:
-        print(piece.name + " : " + str(myGame.findMoves(piece)) )
+    myGame.gameLoop()
 
 main()
